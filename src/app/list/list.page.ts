b4968cf3aad9fadae29b44df37cb8f229ed49d40
Samet Acar <samet@evam.com>
2020-09-23 18:25:49 +0300

    j

--- src/app/list/list.page.ts
+++ src/app/list/list.page.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ServicesService } from '../services.service';
-import { NavController } from '@ionic/angular';
+import { NavController, LoadingController, ToastController } from '@ionic/angular';
 import { DetailPage } from '../detail/detail.page';
 import { NavigationExtras, Router } from '@angular/router';
+import { async } from '@angular/core/testing';
 
 @Component({
   selector: 'app-list',
@@ -12,16 +13,37 @@ import { NavigationExtras, Router } from '@angular/router';
 export class ListPage implements OnInit {
 
   list = [];
+  // loading;
 
-  constructor(private service: ServicesService, private navCtrl: NavController, private router: Router) { }
+  constructor(private service: ServicesService, private navCtrl: NavController, private router: Router, 
+              private loadingCtrl: LoadingController, private toastCtrl: ToastController) { }
+
+  presentLoading() {
+    this.loadingCtrl.create({  })
+    .then((res) => {
+      res.present();
+   })
+  }
 
   getList(){
+    this.presentLoading();
     this.service.serGetListe()
       .then((data: Array<any>) => {
-     
+
         let tags = data['tags'];
         this.list = tags;
-  
+
+        this.loadingCtrl.dismiss().then((res) => { })
+        .catch((error) => {
+        });
+
+       this.toastCtrl.create({
+          message: 'Your List has been getting successfully.',
+          duration: 2000
+        }).then(res => {
+          res.present();
+        });
+        
       }) 
       .catch((err) => {})
   }
